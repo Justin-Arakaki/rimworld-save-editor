16e1a2cf3418c0e50a83c5a46728846c9c3f27cf
Justin-Arakaki <Justiniankoi@gmail.com>
2023-03-06 15:36:44 -0800

    Schedule patches and file generated

--- frontend/src/services/modify-save.ts
+++ frontend/src/services/modify-save.ts
@@ -2,8 +2,13 @@ import sampleSave from '../data/sampleSave.rws'
 import sampleDefaults from '../data/sampleDefaults.rws';
 import createXMLDOC from '../utils/create-xml-doc';
 import replaceXMLChildren from '../utils/replace-xml-children';
+import createRWSFile from '../utils/create-rws-file';
 
 export default async function modifySave() {
   const saveFile = await createXMLDOC(sampleSave);
-  const customSchedule = await createXMLDOC(sampleDefaults);
+  const defaults = await createXMLDOC(sampleDefaults);
+  const schedule = defaults.getElementsByTagName('schedule')[0];
+  replaceXMLChildren(saveFile, schedule);
+  console.log(saveFile.getElementsByTagName('schedule')[0].innerHTML)
+  createRWSFile(saveFile);
 }
--- frontend/src/utils/replace-xml-children.ts
+++ frontend/src/utils/replace-xml-children.ts
@@ -1,7 +1,9 @@
 export default function replaceXMLChildren(
-  elements: HTMLCollection,
+  doc: XMLDocument,
   replacement: Element
 ) {
+  const elementName = replacement.tagName;
+  const elements = doc.getElementsByTagName(elementName);
   for (let element of elements) {
     element.replaceChildren(...replacement.children);
   }
